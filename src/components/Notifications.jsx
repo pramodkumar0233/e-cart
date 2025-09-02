3e4bfa3a5a1f0e2ce20df05c942c05fa3ab76b27
pramodkumar0233 <bhumandla849@gmail.com>
2025-09-02 10:23:57 +0530

    fix in notification alignment

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -6,12 +6,12 @@ const Notifications = () => {
             {notifications.map((item) => (
                 <div key={item.id} className=" text-black m-3 shadow-xl p-4 border-1 rounded-2xl">
                     <div className="text-left">
-                        <span className="bg-blue-500 p-2 border-1 rounded-3xl  bg-gradient-to-r from-blue-500 to-purple-500">#{item.type}</span>
+                        <span className="bg-blue-500 p-2 border-1 rounded-3xl   bg-gradient-to-r from-blue-500 to-purple-500">#{item.type}</span>
                     </div>
-                    <div>
+                    <div className="mt-2">
                         <h1>{item.message}</h1>
                     </div>
-                    <div>
+                    <div className="flex justify-end">
                         <h1>{item.time}</h1>
                     </div>
                 </div>
